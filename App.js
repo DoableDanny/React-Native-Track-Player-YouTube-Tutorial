0d1e92c0eb05a8ff487ad9966ac342d8d77853b8
Dan <dantheado@gmail.com>
2021-02-19 21:21:51 +0000

    Initial commit: project complete

--- App.js
+++ App.js
@@ -2,28 +2,38 @@ import React, {useEffect} from 'react';
 import TrackPlayer from 'react-native-track-player';
 import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
 
-const App = () => {
-  const tracks = [
-    {
-      id: 1,
-      url: require('./tracks/blues.wav'),
-      title: 'Blues Beat',
-    },
-    {
-      id: 2,
-      url: require('./tracks/country.mp3'),
-      title: 'Blues Beat',
-    },
-  ];
+const tracks = [
+  {
+    id: 1,
+    url: require('./tracks/blues.wav'),
+    title: 'Blues Beat',
+  },
+  {
+    id: 2,
+    url: require('./tracks/country.mp3'),
+    title: 'Blues Beat',
+  },
+];
 
-  TrackPlayer.updateOptions({
-    stopWithApp: false,
-    capabilities: [TrackPlayer.CAPABILITY_PLAY, TrackPlayer.CAPABILITY_PAUSE],
-    compactCapabilities: [
-      TrackPlayer.CAPABILITY_PLAY,
-      TrackPlayer.CAPABILITY_PAUSE,
-    ],
-  });
+TrackPlayer.updateOptions({
+  stopWithApp: false,
+  capabilities: [TrackPlayer.CAPABILITY_PLAY, TrackPlayer.CAPABILITY_PAUSE],
+  compactCapabilities: [
+    TrackPlayer.CAPABILITY_PLAY,
+    TrackPlayer.CAPABILITY_PAUSE,
+  ],
+});
+
+const App = () => {
+  const setUpTrackPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add(tracks);
+      console.log('Tracks added');
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   useEffect(() => {
     setUpTrackPlayer();
@@ -31,34 +41,31 @@ const App = () => {
     return () => TrackPlayer.destroy();
   }, []);
 
-  const setUpTrackPlayer = () => {
-    TrackPlayer.setupPlayer()
-      .then(() => {
-        TrackPlayer.add(tracks).then(function () {
-          console.log('Tracks added');
-        });
-      })
-      .catch((e) => console.log(e));
-  };
-
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => {
-          console.log(TrackPlayer.STATE_READY);
-          TrackPlayer.play();
-        }}>
-        <Text style={styles.text}>Play</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => TrackPlayer.pause()}>
-        <Text style={styles.text}>Pause</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => TrackPlayer.skipToNext()}>
-        <Text style={styles.text}>Skip</Text>
-      </TouchableOpacity>
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.pause()}>
+          <Text style={styles.text}>Pause</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => TrackPlayer.play()}>
+          <Text style={styles.text}>Play</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.skipToPrevious()}>
+          <Text style={styles.text}>Prev</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.skipToNext()}>
+          <Text style={styles.text}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -73,14 +80,18 @@ const styles = StyleSheet.create({
   btn: {
     backgroundColor: '#ff0044',
     padding: 15,
-    borderRadius: 15,
+    borderRadius: 10,
     margin: 10,
-    width: 200,
+    width: 160,
   },
   text: {
     fontSize: 30,
     color: 'white',
     textAlign: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 20,
   },
 });
 
